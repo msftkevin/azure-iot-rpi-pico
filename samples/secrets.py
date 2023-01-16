55862fa3656c1261c1d8d7821a87a522fbae3ddf
msftkevin <keorba@microsoft.com>
2023-01-16 11:49:06 -0800

    Updated the secrets file with more comments

--- samples/secrets.py
+++ samples/secrets.py
@@ -1,19 +1,29 @@
+#------------------------------------------------------------------------------
 # WiFi Settings
 SSID = '<SSID>'
 PASSWORD = '<SSID_PASSWORD>'
+#------------------------------------------------------------------------------
 
+
+#------------------------------------------------------------------------------
 # IoT Hub Settings
 # Use the following values to configure your IoT Hub connection 
+# Replace the following items with your IoT Hub values
+# <IOTHUB_CLIENTID> :   This is the 'Device ID' from your IoT Hub Device
+# <IOTHUB_HOSTNAME> :   This is the 'Hostname' from your IoT Hub Overview tab
+# <IOTHUB_PASSWORD> :   This is the 'Connection string with SAS token'.
+#                       The connection string must be generated, which can be done
+#                       with the Azure IoT Hub Explorer.
+#                       https://learn.microsoft.com/en-us/azure/iot-fundamentals/howto-use-iot-explorer
 IOTHUB_CLIENTID = '<CLIENT_ID>'
 IOTHUB_HOSTNAME = '<IOT_HUB_HOSTNAME>'
-
-IOTHUB_USERNAME = '{hostname}/{clientid}/?api-version=2021-04-12'.format(hostname = IOTHUB_HOSTNAME, clientid = IOTHUB_CLIENTID)
 IOTHUB_PASSWORD = '<SHARED_ACCESS_SIGNATURE_TOKEN>'
+IOTHUB_USERNAME = '{hostname}/{clientid}/?api-version=2021-04-12'.format(hostname = IOTHUB_HOSTNAME, clientid = IOTHUB_CLIENTID)
 IOTHUB_TOPIC_PUB = b'devices/{clientid}/messages/events/'.format(clientid = IOTHUB_CLIENTID)
-#IOTHUB_TOPIC_MSG = b'{"buttonpressed":"1"}'
 
 # IoT Hub using MQTT Port 8883 for secure connection
 # Leave IOTHUB_PORT_NO = 0 and the connection will default to 8883
 IOTHUB_PORT_NO = 0
 IOTHUB_SUBSCRIBE_TOPIC = "devices/{clientid}/messages/devicebound/#".format(clientid = IOTHUB_CLIENTID)
-
+#IOTHUB_TOPIC_MSG = b'{"buttonpressed":"1"}'
+#------------------------------------------------------------------------------
